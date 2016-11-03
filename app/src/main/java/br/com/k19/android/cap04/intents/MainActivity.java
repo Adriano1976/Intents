76cbdbb6b16b2a2def151ed1102758f371343fb1
Adriano Santos <adriradiologista@hotmail.com.br>
2016-11-03 03:11:13 -0300

    Usando o Intent com Activities
    Intent são objetos responsáveis por passar informações, como se fossem
    mensagens, para os principais componentes da API do Android, como as
    Activities, Services e BroadCast Receivers.
    
    Para que um desdes componentes seja instanciado, é necessário que seja
    criado um Instant, mesmo quando não temos nenhuma informação para passar
    para o componente criado.
    
    Neste exemplo está sendo mostrado como é feito a aplicação e de que
    forma é usado o Intents.

---
+++ app/src/main/java/br/com/k19/android/cap04/intents/MainActivity.java
@@ -0,0 +1,26 @@
+package br.com.k19.android.cap04.intents;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.main_button);
+
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/br/com/k19/android/cap04/intents/SecondActivity.java
@@ -0,0 +1,14 @@
+package br.com.k19.android.cap04.intents;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+}
